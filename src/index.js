02233be25366cdb42ba929d62de9f0088f2f0763
dinasing <48682243+dinasing@users.noreply.github.com>
2019-10-28 21:01:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 module.exports = function multiply(first, second) {
-  var length = (+first) + (+second) > 1000 ? length = 1000 : length = (+first) + (+second);
-	var result = Array(length).fill(0);
+  let length = (+first) + (+second) > 1000 ? length = 1000 : length = (+first) + (+second);
+  let result = Array(length).fill(0);
   first =  first.split('').reverse();
   second = second.split('').reverse();
   for (let i = 0; i< first.length; i++) {
 	  for (let j = 0; j < second.length; j++) {
-			result[i + j] += first[i] * second[j];
+		result[i + j] += first[i] * second[j];
 	  }
  }
   for(let i = 0 ; i < length-1 ; i++) {
